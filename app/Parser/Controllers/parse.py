7ae237a26ed8a3d4ecaaf28754d9b7a16ef025fb
Viktor <d3nydb@gmail.com>
2021-09-07 12:44:05 +0300

    fix

--- app/Parser/Controllers/parse.py
+++ app/Parser/Controllers/parse.py
@@ -60,8 +60,9 @@ def parseBook(uri, domain):
     page = BeautifulSoup(soup.read(), 'lxml')
     td_center_color = page.find_all('tr', class_='td_center_color')
     # text = td_center_color[0].find('p').text
-    text = td_center_color[0].find('p').get_text(strip=True, separator='|').split('|')
-
+    text = td_center_color[0].find('p').get_text(strip=True, separator='$$').split('$$')
+    print(text)
+    exit()
     book = {}
     book['search'] = {}
     book['database'] = {}
